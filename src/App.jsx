38a0cfca73494731aec50b42d0dffbffdc39e820
erick-costa <erickcosta2509@gmail.com>
2024-07-24 10:53:05 -0300

    project initialized

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Header } from "./components/Header"
+import { Post } from "./Post"
+
+export function App() {
+  return (
+    <>
+      <Header />
+      <Post
+        author="Erick Costa"
+        content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quis numquam eos alias maiores voluptatem maxime, dicta fugiat porro eius blanditiis explicabo debitis, minus enim. Quo explicabo nulla quis mollitia corrupti!"
+      />
+      <Post author="Diego Fernandes" content="Um post legal!" />
+    </>
+  )
+}
