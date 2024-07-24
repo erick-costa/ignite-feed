e15f37dc1631672311df856ffeff6cc803268823
erick-costa <erickcosta2509@gmail.com>
2024-07-24 13:07:37 -0300

    sidebar finished

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,3 +1,4 @@
+import { PencilLine } from "@phosphor-icons/react"
 import styles from "./Sidebar.module.css"
 
 export function Sidebar() {
@@ -9,12 +10,19 @@ export function Sidebar() {
       />
 
       <div className={styles.profile}>
+        <img
+          className={styles.avatar}
+          src="https://github.com/erick-costa.png"
+        />
         <strong>Erick Costa</strong>
         <span>Frontend Developer</span>
       </div>
 
       <footer>
-        <a href="#">Editar seu perfil</a>
+        <a href="#">
+          <PencilLine size={20} />
+          Editar seu perfil
+        </a>
       </footer>
     </aside>
   )
