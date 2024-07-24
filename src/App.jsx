85390436cffbbc365365f01de91cfa17e3ae2e92
erick-costa <erickcosta2509@gmail.com>
2024-07-24 12:24:46 -0300

    component sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
-import { Header } from "./components/Header"
+import { Header } from "./components/header/Header"
 import { Post } from "./Post"
 import "./global.css"
+import styles from "./App.module.css"
+import { Sidebar } from "./components/sidebar/Sidebar"
 
 export function App() {
   return (
-    <>
+    <div>
       <Header />
-      <Post
-        author="Erick Costa"
-        content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quis numquam eos alias maiores voluptatem maxime, dicta fugiat porro eius blanditiis explicabo debitis, minus enim. Quo explicabo nulla quis mollitia corrupti!"
-      />
-      <Post author="Diego Fernandes" content="Um post legal!" />
-    </>
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Erick Costa"
+            content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quis numquam eos alias maiores voluptatem maxime, dicta fugiat porro eius blanditiis explicabo debitis, minus enim. Quo explicabo nulla quis mollitia corrupti!"
+          />
+          <Post author="Diego Fernandes" content="Um post legal!" />
+        </main>
+      </div>
+    </div>
   )
 }
