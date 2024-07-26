248bbe6fb7d2bf2f216b2e4546d8a2ada74e3692
erick-costa <erickcosta2509@gmail.com>
2024-07-26 00:34:12 -0300

    migration to TypeScript

---
+++ src/components/sidebar/Sidebar.tsx
@@ -0,0 +1,27 @@
+import { PencilLine } from "@phosphor-icons/react"
+import styles from "./Sidebar.module.css"
+import { Avatar } from "../avatar/Avatar"
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1475274047050-1d0c0975c63e?q=50&w=2072&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      />
+
+      <div className={styles.profile}>
+        <Avatar src="https://github.com/erick-costa.png" />
+        <strong>Erick Costa</strong>
+        <span>Frontend Developer</span>
+      </div>
+
+      <footer>
+        <a href="#">
+          <PencilLine size={20} />
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  )
+}
